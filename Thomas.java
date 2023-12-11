9833705635d75b42e480c1ee348d68d1dc470b79
villasvildmand <143394123+villasvildmand@users.noreply.github.com>
2023-12-11 20:01:53 +0100

    Små forbedringer

--- Thomas.java
+++ Thomas.java
@@ -19,44 +19,32 @@ public class Thomas extends JuniorRobot
 
 		// Some color codes: blue, yellow, black, white, red, pink, brown, grey, orange...
 		// Sets these colors (robot parts): body, gun, radar, bullet, scan_arc
-		setColors(orange, blue, white, yellow, black);
+		setColors(0xFF00FF, 0xFF00FF, 0xFF00FF, yellow, black);
 
 		// Robot main loop
 		while(true) {
-			back(10);
+			ahead(100);
+			turnRight(90);
 		}
 	}
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
 	public void onScannedRobot() {
-		// Replace the next line with any behavior you would like
-		fire(1);
+		turnTo(scannedAngle);
+		fire(3);
 	}
 
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
 	public void onHitByBullet() {
-		// Replace the next line with any behavior you would like
-		back(10);
+		turnTo(hitByBulletAngle);
 	}
 	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
 	public void onHitWall() {
-		// Replace the next line with any behavior you would like
 		back(10);
 		turnRight(hitWallAngle);
-		//out.println("I HIT A WALL");
 	}	
 	
 	public void onPaint(Graphics2D g) {
-		System.out.println("HELLO");
-		//Rules.getBulletSpeed();
-		g.setColor(new Color(0xff, 0xff, 0x00, 0x80));
+		//Virker ikke?
+		g.setColor(Color.RED);
 		g.drawLine(robotX, robotY, 200, 200);
 	}	
 }
