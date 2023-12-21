8808950d18edbb17dc6841f73e7c6141ed3cda76
Willi2606 <112610212+Willi2606@users.noreply.github.com>
2023-12-21 12:40:57 +0100

    Add files via upload

---
+++ Adolfs_Kampfwagen.java
@@ -0,0 +1,62 @@
+package H23HX3;
+import robocode.*;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/JuniorRobot.html
+
+/**
+ * Adolfs_Kampfwagen - a robot by (your name here)
+ */
+public class Adolfs_Kampfwagen extends JuniorRobot
+{
+	/**
+	 * run: Adolfs_Kampfwagen's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// Some color codes: blue, yellow, black, white, red, pink, brown, grey, orange...
+		// Sets these colors (robot parts): body, gun, radar, bullet, scan_arc
+		setColors(orange, blue, white, yellow, black);
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot() {
+		// Replace the next line with any behavior you would like
+		System.out.println(gunHeading);
+		System.out.println(scannedAngle);
+		turnGunTo(scannedAngle);
+		fire(1);
+		ahead(400);
+		turnRight(90);
+		ahead(250);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet() {
+		// Replace the next line with any behavior you would like
+		back(120);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall() {
+		// Replace the next line with any behavior you would like
+		//Finder nuværende position
+		ahead(-250);
+		turnRight(180);
+	}	
+}
